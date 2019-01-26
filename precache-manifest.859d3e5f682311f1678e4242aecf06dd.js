91c2a454d6bf71bdc6870607eae501efafcabfb6
kabyanil <kabyaniltalukdarindia@gmail.com>
2019-01-26 12:22:58 +0530

    committed all files to local repo

---
+++ precache-manifest.859d3e5f682311f1678e4242aecf06dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b57d3490ba61b9e195146829055ced9b",
+    "url": "/static/media/Amitabh Gohain.b57d3490.jpg"
+  },
+  {
+    "revision": "5cba86269b1f8239c975",
+    "url": "/static/css/main.77c69d6b.chunk.css"
+  },
+  {
+    "revision": "414567c6750de5ea59a190568224c77f",
+    "url": "/static/media/bg.414567c6.png"
+  },
+  {
+    "revision": "85d5ee0e2f8e4049abf6",
+    "url": "/static/js/1.85d5ee0e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5cba86269b1f8239c975",
+    "url": "/static/js/main.5cba8626.chunk.js"
+  },
+  {
+    "revision": "6ea02f45ab6f9eb35641b062e296cace",
+    "url": "/static/media/Zublee Baruah.6ea02f45.png"
+  },
+  {
+    "revision": "0bbc3c83b31f08fd7eba765db662cfed",
+    "url": "/static/media/Priyanka Borah.0bbc3c83.jpg"
+  },
+  {
+    "revision": "2a3de1e51239a1a0166ec9d9342e5d86",
+    "url": "/static/media/Partha Pratim Goswami.2a3de1e5.jpg"
+  },
+  {
+    "revision": "85d5ee0e2f8e4049abf6",
+    "url": "/static/css/1.73db3363.chunk.css"
+  },
+  {
+    "revision": "3774e831f372263e5317e80d96618a34",
+    "url": "/index.html"
+  }
+];
